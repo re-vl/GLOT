3a6e715c52b24cecef5b87ad20208ced1f0dcaba
Vladimir <rechmed@yandex.ru>
2021-10-30 08:50:33 +0300

    animation elements added

--- dist/js/script.js
+++ dist/js/script.js
@@ -44,9 +44,7 @@ $(document).ready(function () {
          messages: {
             name: {
                required: "Введите ваше имя",
-               minlength: jQuery.validator.format(
-                  "Введите {0} или более символа!"
-               ),
+               minlength: jQuery.validator.format("Введите {0} или более символа!"),
             },
             phone: "Введите номер вашего телефона",
             email: {
@@ -93,4 +91,44 @@ $(document).ready(function () {
       $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
       return false;
    });
+   //анимации скролла
+   const animItems = document.querySelectorAll(".anim-items");
+
+   if (animItems.length > 0) {
+      window.addEventListener("scroll", animOnScroll);
+      function animOnScroll() {
+         for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 3;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+               animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if (
+               window.pageYOffset > animItemOffset - animItemPoint &&
+               window.pageYOffset < animItemOffset + animItemHeight
+            ) {
+               animItem.classList.add("active");
+            } else {
+               if (!animItem.classList.contains("anim-no-hide")) {
+                  animItem.classList.remove("active");
+               }
+            }
+         }
+      }
+      function offset(el) {
+         const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+         return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+      }
+      setTimeout(() => {
+         animOnScroll();
+      }, 300);
+   }
 });
